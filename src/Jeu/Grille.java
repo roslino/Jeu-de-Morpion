fef84ddb9c2f9cb40a8f8853fa85f855c7f790b5
Guy-Ross <guyrossaf@yahoo.fr>
2020-08-09 15:06:51 -0400

    Suppression de la classe de test et ajout de la methode main() dans la classe Grille

--- src/Jeu/Grille.java
+++ src/Jeu/Grille.java
@@ -53,4 +53,24 @@ public class Grille {
 
     // deplacer()
 
+    // class main
+    public static void main(String[] args) {
+
+        Grille g = new Grille(12, 8);
+
+        g.placer(3, 5, 'J');
+        g.placer(8, 2, 'O');
+        g.placer(1, 1, 'M');
+        g.placer(7, 4, 'O');
+        g.placer(10, 7, 'M');
+        g.placer(2, 8, 'O');
+        g.placer(11, 2, 'M');
+
+        g.afficher();
+        //System.out.println("J = Joueur\nO = Objet\nM = Monstre\n. = Vide");
+        int case1 = g.getCase(3, 5);
+        System.out.println(g.getCase(3, 5));
+
+    }
+
 }
